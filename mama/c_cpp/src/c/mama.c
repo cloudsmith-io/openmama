2c7baf62b4d587d35885de3dde86dac9bf590abc
Lee Skillen <lskillen@vulcanft.com>
2014-02-24 19:05:22 +0000

    MAMA: Expose open/close reference counting + tidyup reserved fields.
    - Fixed two types of leaks for reserved fields:
    
      (1) MamaCPP: Fields weren't being destroyed on the final (or any)
          Mama::close().
    
      (2) MamaCPP: Field initialisation was called on every Mama::open(),
          causing lost pointers on every successive call.
    
    - Simplified the init/destroy of reserved fields within MamaC and
      MamaCpp via the use of macros - This was mainly due to the addition of
      additional protection to ensure calls to init/destroy were always
      idempotent so it doesn't matter how many times you call either (esp.
      because the init/destroy functions are exposed to users).
    
    - Exposed the reference counting versions of MamaC/MamaCPP open/close,
      which was primarily used to implement the balanced calls to reserved
      fields init/destroy.  We actually use these for some other one-time
      setup and destroy at Vulcan, so might be useful for others.  Docs
      were also updated to be consistent across versions (needs checked).
    
    - Added calls to cleanup reserved fields if MamaC open() fails.
    
    Additional changes:
    
    - Fixed some memory leaks with MamaCPP unit tests.

--- mama/c_cpp/src/c/mama.c
+++ mama/c_cpp/src/c/mama.c
@@ -642,7 +642,7 @@ mamaInternal_getAllowMsgModify (void)
     return gAllowMsgModify;
 }
 
-static mama_status
+mama_status
 mama_openWithPropertiesCount (const char* path,
                               const char* filename,
                               unsigned int* count)
@@ -694,8 +694,7 @@ mama_openWithPropertiesCount (const char* path,
 
     if (0 != gImpl.myRefCount)
     {
-        if (MAMA_STATUS_OK == result)
-            gImpl.myRefCount++;
+        gImpl.myRefCount++;
         
         if (count)
             *count = gImpl.myRefCount;
@@ -768,6 +767,8 @@ mama_openWithPropertiesCount (const char* path,
 
     if (0 == numBridges)
     {
+        cleanupReservedFields();
+
         mama_log (MAMA_LOG_LEVEL_SEVERE,
                   "mama_openWithProperties(): "
                   "At least one bridge must be specified");
@@ -780,6 +781,8 @@ mama_openWithPropertiesCount (const char* path,
 
     if (!gDefaultPayload)
     {
+        cleanupReservedFields();
+
         mama_log (MAMA_LOG_LEVEL_SEVERE,
                   "mama_openWithProperties(): "
                   "At least one payload must be specified");
@@ -987,6 +990,14 @@ mama_open ()
 }
 
 mama_status
+mama_openCount (unsigned int* count)
+{
+    /*Passing NULL as path and filename will result in the
+     default behaviour - mama.properties on $WOMBAT_PATH*/
+    return mama_openWithPropertiesCount (NULL, NULL, count);
+}
+
+mama_status
 mama_openWithProperties (const char* path,
                          const char* filename)
 {
@@ -1098,7 +1109,7 @@ mama_getVersion (mamaBridge bridgeImpl)
     return mama_ver_string;
 }
 
-static mama_status
+mama_status
 mama_closeCount (unsigned int* count)
 {
     mama_status    result     = MAMA_STATUS_OK;
